628fdcec5a2ca37c806d2aa6b1c72e4de153fdb3
Filipe Lima <131200594+FilipeLiima@users.noreply.github.com>
2024-03-20 20:21:21 -0300

    up

---
+++ src/RENT.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract RENT is ERC20 {
+    address owner;
+
+        constructor() ERC20("RENT", "RNT") {
+            owner = msg.sender;
+            mint(msg.sender, 500000 * 10 * 18);
+    }
+    modifier onlyOwner () {
+        require(msg.sender == owner, "GFT: Only owner");
+        _;
+    }
+}
