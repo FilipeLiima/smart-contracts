7d876860a922e4c64b88bb0a4f793c84d8864842
Filipe Lima <131200594+FilipeLiima@users.noreply.github.com>
2024-03-22 10:55:36 -0300

    preparando para deploy

--- script/RENT.s.sol
+++ script/RENT.s.sol
@@ -5,10 +5,14 @@ import {Script, console2} from "forge-std/Script.sol";
 import {RENT} from "../src/RENT.sol";
 
 contract RENTScript is Script {
+    RENT public rent; // Declaração da variável rent
 
     function setUp() public {}
 
     function run() public {
-        vm.broadcast();
+        vm.startBroadcast(0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80);
+        rent = new RENT();
+        console2.log("Address: ", address(rent)); // Correção na impressão do endereço
+        vm.stopBroadcast();
     }
 }
