8602565409c309c85049ae2726f70a6a59a78971
Filipe Lima <131200594+FilipeLiima@users.noreply.github.com>
2024-03-22 14:54:47 -0300

    deploy ERC721

--- script/NFT_RENT.s.sol
+++ script/NFT_RENT.s.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.13;
 
 import {Script, console2} from "forge-std/Script.sol";
 import {NFT_RENT} from "../src/NFT_RENT.sol";
+import {RENT} from "../src/RENT.sol";
 
 contract NFT_RENTScript is Script {
     NFT_RENT public rent; 
@@ -11,8 +12,8 @@ contract NFT_RENTScript is Script {
 
     function run() public {
         vm.startBroadcast(0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80);
-        rent = new NFT_RENT();
-        
+        rent = new NFT_RENT(address(new RENT()), 500); // Criar nova instância do contrato RENT
         console2.log("Address: ", address(rent));
+        vm.stopBroadcast();
     }
 }
